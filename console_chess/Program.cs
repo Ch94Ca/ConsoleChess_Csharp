789688ab8f59cb62a12e04efb1cef16f921c94be
Carlos Araújo <ch94ca@gmail.com>
2021-08-11 12:14:02 -0300

    Position class created

---
+++ console_chess/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Board;
+
+namespace console_chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(2, 3);
+            Console.WriteLine(p);
+        }
+    }
+}
